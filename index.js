890beb51cb716490ed3220f3f34f1f37e38f2e89
TakamakWasTaken <gautier.nicollet@ynov.com>
2018-05-14 10:32:01 +0200

    final version

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const fs = require('fs');
-var date;
-const fileName = 'date.txt'
+const fileName = 'date.txt';
 
-//console.log(date);
-if(fileName.exists)
+function writeAndRead() {
+    fs.appendFile(fileName, Date.now() + '\r\n');
 
-function read() {
     var contents = fs.readFileSync(fileName, 'utf8');
     console.log(contents);
 }
 
-function writeDate() {
-    fs.appendFile(fileName, Date.now() + '\r\n');
-
-    read();
-}
-
-setInterval(writeDate, 1000);
+setInterval(writeAndRead, 1000);
